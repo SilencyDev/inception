0d4e0ed183aa770b882921ba24695bab27dd4ab0
Kevin Macquet <kmacquet@c1r8p1.42nice.fr>
2021-08-16 17:53:34 +0200

    Fixed path issue

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,13 +1,13 @@
-/usr/bin/mysql_install_db --user=root --basedir='/usr' --datadir='/var/data/mysql'
-/usr/bin/mysqld --user=root --datadir='/var/data/mysql' &
+/usr/bin/mysql_install_db --user=root --basedir='/usr' --datadir='/var/lib/mysql'
+/usr/bin/mysqld --user=root --datadir='/var/lib/mysql' &
 sleep 5
 
-mysql -e "CREATE DATABASE IF NOT EXISTS Inception DEFAULT CHARACTER SET utf8;"
-mysql -e "USE Inception;"
-mysql -e "CREATE USER IF NOT EXISTS 'user'@'localhost' IDENTIFIED BY 'password';"
-mysql -e "GRANT ALL PRIVILEGES ON Inception.* TO 'user'@'localhost';"
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_NAME DEFAULT CHARACTER SET utf8;"
+mysql -e "USE $MYSQL_NAME;"
+mysql -e "CREATE USER IF NOT EXISTS $MYSQL_USER@$DOMAIN_NAME IDENTIFIED BY $MYSQL_PASSWORD;"
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_NAME.* TO $MYSQL_USER@$DOMAIN_NAME;"
 mysql -e "FLUSH PRIVILEGES;"
 
 pkill mysqld
 
-/usr/bin/mysqld --user=root --datadir=/var/data/mysql
+/usr/bin/mysqld --user=root --datadir=/var/lib/mysql
