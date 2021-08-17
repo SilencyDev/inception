845bc736a80e4c018925807b779c144f6b002b78
Kevin Macquet <kmacquet@c1r8p1.42nice.fr>
2021-08-17 18:44:38 +0200

    Added wp-cli installation

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,3 +1,7 @@
 mv ./wp-config.php /var/www/localhost/wordpress/
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+wp --allow-root core install --url="localhost/wordpress" --path="/var/www/localhost/wordpress/" --title="a title" --admin_user='user' --admin_password='password' --admin_email="test@test.fr"
 
 exec php-fpm7 -F;
