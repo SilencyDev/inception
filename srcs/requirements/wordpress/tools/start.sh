3a8f428267b4ae6fd7bad622f23783444c5e0f62
Kevin Macquet <kvn.macquet@gmail.com>
2021-08-18 19:17:06 +0200

    Added user with lower priviledge for wp

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -3,6 +3,7 @@ mv ./wp-config.php /var/www/localhost/wordpress/
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-wp --allow-root core install --url="$DOMAIN_NAME" --path="/var/www/localhost/wordpress/" --title="a title" --admin_user='user' --admin_password='password' --admin_email="test@test.fr"
-
+sleep 7
+wp --allow-root core install --url="$DOMAIN_NAME" --path="/var/www/localhost/wordpress/" --title="a title" --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASSWORD --admin_email="test@test.fr"
+wp --allow-root user create $WP_USER_NAME "test2@test.fr" --user_pass=$WP_USER_PASSWORD --role='author' --path="/var/www/localhost/wordpress/"
 exec php-fpm7 -F;
